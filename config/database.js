ed78ef73f59ce5663c7ef8aa00039e683b49aa4c
Omar <37917804+anaomart@users.noreply.github.com>
2024-09-18 01:45:20 +0300

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 const mongoose = require('mongoose');
-let url = "mongodb+srv://<omar1>:<xr5Iyl2DLbzYs5Xe>@sara7a.fed2vyk.mongodb.net/test"
 module.exports.dbConnection = () => {
     mongoose.connect(url)
         .then(() => console.log('Connection established ..'));
